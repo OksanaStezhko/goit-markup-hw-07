8ef1a1bd9223742e7dbf971e420cf51a030a2fe6
Oksana Stezhko <oksana_stegko@ukr.net>
2020-09-20 22:05:57 +0300

    исправляю ошибки

--- js/close.js
+++ js/close.js
@@ -1,14 +1,14 @@
-<script>
-    const inputs = document.querySelectorAll("input[type=checkbox]")
-    const button = document.querySelector(".button-submit")
-            function check() {
-        button.disabled = true
-                inputs.forEach(input => {
-                    if (input.checked) button.disabled = false
-                    // else {
-        //  button.disabled = true
-        // }
-    })
-            }
-            inputs.forEach(input => input.addEventListener("change", check))
-        </script>
+(() => {
+  const inputs = document.querySelectorAll('input[type=checkbox]');
+  const button = document.querySelector('.button-submit');
+  function check() {
+    button.disabled = true;
+    inputs.forEach(input => {
+      if (input.checked) button.disabled = false;
+      else {
+        button.disabled = true;
+      }
+    });
+  }
+  inputs.forEach(input => input.addEventListener('change', check));
+})();
